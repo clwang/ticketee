324c809015951de65913b77ae4497ec492ef4d47
Charles Wang <charleswang3@gmail.com>
2012-08-26 15:16:44 -0700

    assets are now strictly served through FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Ticketee::Application.routes.draw do
 
   get '/awaiting_confirmation', :to => "users#confirmation", :as => "confirm_user"
   put '/admin/users/:user_id/permissions', :to => "admin/permissions#update", :as => "update_user_permissions"
+
+  resources :files
+
   resources :projects do
     resources :tickets
   end
